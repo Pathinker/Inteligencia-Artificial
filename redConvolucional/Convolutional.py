545c4392b61f95d46180f52efb6d6c94094a903e
DarchoG <danielguzmanzavala004@gmail.com>
2024-09-08 20:41:50 -0600

    Implementación del forward propagation

---
+++ redConvolucional/Convolutional.py
@@ -0,0 +1,55 @@
+import numpy as np
+from Layer import Layer
+from scipy import signal
+
+# Una red convolucional emplea la correlación cruzada "cross correlation" de mis matrices de inputs con un kernel.
+# El kernel es suceptible de disponer de valores arbitrarios que sean útiles para la abstracción de caracteristicas.
+# En otras palabras filtran los datos de entrada, considerando de igual manera el bias o el sezgo.
+
+class Convolutional(Layer):
+
+    def __init__(self, inputShape, kernelSize, depth):
+
+        # Obtener la cantidad de matrices de entrada y sus dimenciones respectivamente, realiza un desempaquetado de los datos.
+        inputDepth, inputHeight, inputWidth = inputShape
+        self.depth = depth
+        self.inputShape = inputShape
+        self.inputDepth = inputDepth
+        # Número de kernels y las dimenciones de las matrices.
+        self.outputShape = (depth, inputHeight - kernelSize + 1, inputWidth - kernelSize + 1)
+        # Numero de matrices de la capa de entrada, ya que a cada matriz le corresponde un kernel de longitud cuadrada.
+        self.kernelsShape = (depth, inputDepth, kernelSize, kernelSize)
+        # Inicializar de manera aleatoria.
+        self.kernels = np.random.rand(*self.kernelsShape)
+        self.biases = np.random.rand(*self.outputShape)
+
+    def forward(self, input):
+
+        # El forward propagation de una red neuronal es una suma ponderada de todos sus neuronas por sus pesos más un sezgo "bias".
+        # En una red neuronal correlacional es repetida con la diferencia de ser matrices en lugar de productos escalares, por ende no es posible decomponerlo en productos matriciales.
+        # Existen diferentes maneras de realizar correlaciones cruzadas.
+        # "Valid" genera una matriz del tamaño del kernel donde solamente es evaluado el kernel respecto al input sin exederse de las longitudes de la matriz.
+        # "Full" calcula todas las correlacones validas y es tomado en consideración los bordes donde valid no evalua, es obtenida una matriz de una mayor dimensión.
+
+        self.input = input
+
+        # En la correlación cruzada siempre es adicionado el sezgo o bias, por ende es copiado.
+
+        self.output = np.copy(self.biases)
+
+        for i in range(self.depth):
+            for j in range(self.depth):
+
+                # Empleamos la libreria spicy para ejecturar la cross correlation valid, es importante denotar que es no es conmutativo.
+                # Al disponer del sezgo es sumdo al resultado de la corss correlation entre los inputs y los kernels.
+                # Nos dara como resultado una matriz del mismo tamaño que el kernel por ende lo ejecutamos n veces siendo la profunidad y n veces siendo la cantidad de datos.
+
+                self.output[i] += signal.correlate2d(self.input[j], self.kernels[i, j], "valid")
+
+        return self.output
+    
+    def backward(self, input):
+
+        # Método de ajuste y aprendizaje sobre la comparación del resultado valido al esperado.
+
+        pass
---
+++ redConvolucional/Layer.py
@@ -0,0 +1,25 @@
+class Layer:
+
+    def __init__(self):
+
+        #Todas las capas estan compuestas por datos de entrada que reciben de capas previas y el output que emanan a la siguiente capa.
+
+        self.input = None
+        self.output = None
+
+        def forward(self, input): 
+            
+            # Forward Propagation, actualiza el valor de las capas subsecuentes acorde el resultado obtenido.
+            # Consiste en una suma ponderada de todas las neuronas por sus pesos (caminos)
+
+            pass
+
+        def backward(self, outputGradient, learningRate): 
+            
+            # Backward Propagration es empleado para actualzar los parametros al ejecutarse el proceso de aprendizaje.
+            # Sera usado el método de gradiente descente que toma en consideración el error del sistema respecto al output.
+            # Gradiente Descendente: Busca Minimos, Gradiente Ascendente: Busca Máximos.
+            # Él método de Gradiente busca llegar a derivadas 0, solamente cambia el signo conforme
+            # learningRate: Es susceptible de incorporar un algoritmo optimizador.
+
+            pass
