f6d39f3a1a6c66346472909c8a1918d25ab65072
Pathinker <danielguzmanzavala004@gmail.com>
2024-11-19 02:52:53 -0600

    Actualización Gráficas

--- weedDetectionInWheat/graficasGWO.py
+++ weedDetectionInWheat/graficasGWO.py
@@ -1,8 +1,8 @@
 import numpy as np
+import tensorflow as tf
 import matplotlib.pyplot as plt
 
-datos = np.loadtxt("weedDetectionInWheat/CNN/MetaheuristicReport.txt", delimiter=",")
-print(datos)
+datos = np.loadtxt("weedDetectionInWheat/CNN/MetaheuristicReport1350.txt", delimiter=",")
 
 epochs = []
 
@@ -26,4 +26,34 @@ plt.plot(epochs, datos[9], label = "Delta", color = "green")
 plt.xlabel("Epochs")
 plt.ylabel("Error")
 plt.legend()
+plt.show()
+
+model = tf.keras.models.load_model('weedDetectionInWheat/CNN/alexnetMetaheuristic1350.keras')
+
+weights = model.get_weights()
+flattenedWeights = np.concatenate([weight.flatten() for weight in weights])
+
+meanValue = np.mean(flattenedWeights)
+variance = np.var(flattenedWeights)
+stdDeviation = np.std(flattenedWeights)
+maxValue = np.max(flattenedWeights)
+minValue = np.min(flattenedWeights)
+
+print(f"Mean: {meanValue}")
+print(f"Variance: {variance}")
+print(f"Standard Deviation: {stdDeviation}")
+print(f"Max Value: {maxValue}")
+print(f"Min Value: {minValue}")
+
+plt.figure(figsize=(10, 6))
+plt.boxplot(flattenedWeights, vert=False)
+plt.title("Box Plot of Weights")
+plt.xlabel("Weight Value")
+plt.show()
+
+plt.figure(figsize=(10, 6))
+plt.hist(flattenedWeights, bins=100, color='skyblue', edgecolor='black')
+plt.title("Weights Histogram")
+plt.xlabel("Weight Value")
+plt.ylabel("Frequency")
 plt.show()
