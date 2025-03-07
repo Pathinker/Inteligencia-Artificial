b4c610d6e8ab16502eaae762fb6eba25d00dc409
Pathinker <danielguzmanzavala004@gmail.com>
2025-03-07 16:05:13 -0600

    Refactorizacion

--- weedDetectionInWheat/CNN/alexnetMetaheuristic.py
+++ weedDetectionInWheat/CNN/alexnetMetaheuristic.py
@@ -7,84 +7,72 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 from sklearn.utils.class_weight import compute_class_weight # type: ignore
 
-# Añadir la carpeta necesaria para importaqr la clase alexnet
-
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
-from weedDetectionInWheat.CNN.alexnetClass import alexnet
 from weedDetectionInWheat.metaheuristic.GWOGPU import GWO
 
-direccionDataset = Path("weedDetectionInWheat/Dataset")
-direccionEntrenamiento = direccionDataset / "train/"
-direccionValidamiento = direccionDataset / "valid/"
+dataset_path = Path("weedDetectionInWheat/Dataset")
+train_path = dataset_path / "train/"
+validation_path = dataset_path / "valid/"
 
 anchoImagen = 227
 largoImagen = 227
-imgSize = [anchoImagen, largoImagen]
-batchSize = 24
+image_size = [anchoImagen, largoImagen]
+batch_size = 24
 
-trainDataFrame = tf.keras.utils.image_dataset_from_directory(
-    direccionEntrenamiento,
+train_data_frame = tf.keras.utils.image_dataset_from_directory(
+    train_path,
     seed=123,
-    image_size=imgSize,
-    batch_size=batchSize,
+    image_size=image_size,
+    batch_size=batch_size,
     label_mode="binary"
 )
 
-validacionDataFrame = tf.keras.utils.image_dataset_from_directory(
-    direccionValidamiento,
+validation_data_frame = tf.keras.utils.image_dataset_from_directory(
+    validation_path,
     seed=123,
-    image_size=imgSize,
-    batch_size=batchSize,
+    image_size=image_size,
+    batch_size=batch_size,
     label_mode="binary"
 )
 
-dataArgumentation = tf.keras.Sequential([
-
-    # Transformaciones Geometricas
-
+data_argumentation = tf.keras.Sequential([
     tf.keras.layers.RandomFlip("horizontal_and_vertical"),
     tf.keras.layers.RandomRotation(0.2),
     tf.keras.layers.RandomZoom(0.2),
     tf.keras.layers.RandomTranslation(0.1, 0.1),
-
-    # Transformaciones Color       
-
-    tf.keras.layers.RandomContrast(0.2),           # Ajuste del contraste
-    tf.keras.layers.RandomBrightness(0.2),         # Ajuste del brillo                               
-                                          
+    tf.keras.layers.RandomContrast(0.2),
+    tf.keras.layers.RandomBrightness(0.2),
 ])
 
-def procesarImagen(x, y):
+def apply_data_argumentation(data_frame):
 
-    return dataArgumentation(x), y
+    def process_image(x, y):
+        return data_argumentation(x), y
 
-# Modificar el dataset.
+    data_argumentation = data_frame.map(process_image)
+    labels = np.concatenate([y for x, y in train_data_frame], axis = 0)
+    labels = labels.flatten()
 
-dataArgumentationTrain = trainDataFrame.map(procesarImagen)
+    return data_argumentation, labels
 
-etiquetasDataset = np.concatenate([y for x, y in trainDataFrame], axis = 0)
+def balance_clases_dataset(labels):
 
-etiquetasDataset = etiquetasDataset.flatten()
+    class_weight = compute_class_weight(class_weight = "balanced",
+                                    classes = np.unique(labels),
+                                    y = labels)
+    unique_classes = np.unique(labels)
+    weights = {}
 
-pesosClases = compute_class_weight(class_weight = "balanced",
-                                   classes = np.unique(etiquetasDataset),
-                                   y = etiquetasDataset)
+    for i in range(len(unique_classes)):
+        weights[int(unique_classes[i])] = float(class_weight[i])
 
-pesosClasesDiccionario = {}
-clasesUnicas = np.unique(etiquetasDataset)
+    return weights
 
-for i in range(len(clasesUnicas)):
-    pesosClasesDiccionario[int(clasesUnicas[i])] = float(pesosClases[i])
-
-alexnetGradiente = keras.models.load_model("weedDetectionInWheat/CNN/alexnetNormalized.keras")
+train_data_argumentation, labels = apply_data_argumentation(train_data_frame)
+class_weights =  balance_clases_dataset(labels)
+model = keras.models.load_model("weedDetectionInWheat/CNN/alexnetNormalized.keras")  # Change path to train other gradient trainned models.
     
-# Inicializar GWO con la estructura de los pesos del modelo
-gwo = GWO(model=alexnetGradiente, epochs=100, agents= 10, wolves = 10, class_weight = pesosClasesDiccionario, feature_selection = "flatten", ensemble_model = True)
-
-# Optimizar con GWO
-alexnetGradiente = gwo.optimize_feature(dataArgumentationTrain, validacionDataFrame)
-
-# Establecer los mejores pesos encontrados al modelo    
-
-alexnetGradiente.save('weedDetectionInWheat/CNN/alexnetMetaheuristic.keras')
+gwo = GWO(model=model, epochs=100, agents= 10, wolves = 10, class_weight = class_weights, feature_selection = "flatten", ensemble_model = True)
+model = gwo.optimize_feature(train_data_argumentation, validation_data_frame)
+model.save('weedDetectionInWheat/CNN/alexnetMetaheuristic.keras')
